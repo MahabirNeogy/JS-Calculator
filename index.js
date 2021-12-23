b90d4f47300d34ab43014f2b123d686cf5b9c20f
Mahabir Neogy <90251647+MahabirNeogy@users.noreply.github.com>
2021-12-23 08:54:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ class Calculator {
             case '*' :
                 computation=prev*current;
                 break;
-            case '/' :
+            case '÷' :
                 computation=prev / current;
                 break;
             default:
